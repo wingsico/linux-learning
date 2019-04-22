dc10d3737a860b7fac66e1f90c7eeb89907cec68
vydVolk <vydVolk@gmail.com>
2019-04-22 15:06:54 +0800

    [makefile]: 学习写一个简单的makefile

--- shell/src/lab4/my_avg.c
+++ shell/src/lab4/my_avg.c
@@ -2,5 +2,10 @@
 
 double my_avg(double *grades, int size)
 {
-  return my_sum(grades, size) / size;
+  double sum = 0;
+  for (int i = 0; i < size; i++)
+  {
+    sum += grades[i];
+  }
+  return sum / size;
 }
--- shell/src/lab4/my_sum.c
+++ shell/src/lab4/my_sum.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-double my_sum(doubel *grades, int size) 
+double my_sum(double *grades, int size) 
 {
   double sum = 0;
   for (int i = 0; i < size; i++)
