37ade0a7d14fae4cf1f76f3695bd63854376a143
vydVolk <vydVolk@gmail.com>
2019-05-09 15:03:48 +0800

    [system]: time

---
+++ system_func/time.cpp
@@ -0,0 +1,23 @@
+#include <ctime>
+#include <iostream>
+#include <cstdlib>
+
+void showGMT(time_t, int time_area);
+
+int main() {
+  time_t t;
+  time(&t);
+  // 格林尼治
+  showGMT(t, 0);
+  // 北京
+  showGMT(t, 8);
+  return 0;
+}
+
+void showGMT(time_t t, int time_area) {
+  using namespace std;
+  struct tm *p;
+  t = t + time_area * 3600;
+  cout << "GMT: " << asctime(gmtime(&t));
+}
+
