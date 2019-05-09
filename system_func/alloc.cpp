9fc1365b9a58ae0306e8e5b14058a6ad926ecef6
vydVolk <vydVolk@gmail.com>
2019-05-09 15:04:23 +0800

    [system]: malloc, calloc

---
+++ system_func/alloc.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#define NUM 10
+
+int main() {
+  int *mp;
+  int max, min;
+
+  mp = (int *)malloc(NUM * sizeof(int));  
+  // mp = (int *)calloc(NUM, sizeof(int));
+  max = min = *mp;
+  
+  std::cout << "input " << NUM << " numbers:" << std::endl;
+  for(int i = 0; i < NUM; i++) {
+    std::cin >> *(mp+i);  
+    if (*(mp+i) > max) {
+      max = *(mp+i);
+    }
+
+    if (*(mp+i) < min) {
+      min = *(mp+i);
+    }
+  }
+
+  std::cout << "max: " << max << ", min: " << min << std::endl;
+  return 0;
+}
